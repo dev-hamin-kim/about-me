87a32e2582a2e30977215418604f507a9ba61a3a
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-12-05 14:51:04 +0900

    [chore] added base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/about-me/",
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
